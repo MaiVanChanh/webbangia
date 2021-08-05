0659ced63952f39b0e83fb9f2a20ca2b407b6e5b
Mai Văn Chánh <77136941+MaiVanChanh@users.noreply.github.com>
2021-08-05 20:30:49 +0700

    Update ketnoi.php

--- include/ketnoi.php
+++ include/ketnoi.php
@@ -1,11 +1,11 @@
 <?php
-	 $ung=mysqli_connect("localhost","root","") or die('Khong The Ket Noi Voi May Chu');
+	 $ung=mysqli_connect("localhost","root","") or die('Không thể kết nối với máy chủ');
 	 $strSQL=mysqli_select_db ($ung,"giayda");
 	mysqli_query($ung,"SET NAMES 'utf8'");
  
 
-$server_username = "root";
-$server_password = "";
+$server_username = "bbea14e2dbb23f";
+$server_password = "f09cbd95";
 $server_host = "localhost"; 
 $database = "giayda"; 
 $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
